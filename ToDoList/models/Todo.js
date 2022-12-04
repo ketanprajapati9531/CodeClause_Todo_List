0d8e50aee43284ce792559f80c6b98249f8bea61
ketan prajapati <109566171+ketanprajapati9531@users.noreply.github.com>
2022-12-04 11:49:14 +0530

    Add files via upload

---
+++ ToDoList/models/Todo.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const TodoSchema = new mongoose.Schema({
+  todo: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = new mongoose.model("Todo", TodoSchema);
+
+
+
+//ketan prajapati
+//CodeClause
---
+++ ToDoList/routes/index.js
@@ -0,0 +1,16 @@
+const router = require("express").Router()
+const Todo = require("../models/Todo");
+
+// routes will be here....
+router.get("/", async(req, res) => {
+    const allTodo = await Todo.find();
+    res.render("index", {todo: allTodo})
+})
+
+
+module.exports = router;
+
+
+
+//ketan prajapati
+//CodeClause
